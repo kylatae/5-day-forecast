901a982f49b786f0db31b0064f60d11f3136d6cd
Kylatae <arthur.henningfield@gmail.com>
2023-10-16 21:38:08 -0500

    Fixed search history and able to search by city, state or by zipcode now

---
+++ README.md
@@ -0,0 +1,26 @@
+# 5 Day Forecast
+
+Weather Forcast for current and next 5 days of weather using openweathermap API and zippopotam API
+ 
+## Description
+
+Simple weather forecast search that allows you to find a city by city and state or zipcode giving back current and future weather. Saves a history of up to the last 5 searches. 
+
+### Preview Page
+
+![site review](./assets/images/readme.png)
+
+Page Link:[https://kylatae.github.io/5_day_forecast/](https://kylatae.github.io/5_day_forecast/) 
+
+Repo Link:[https://www.github.com/kylatae/5_day_forecast/](https://www.github.com/kylatae/5_day_forecast/)
+
+
+## Authors & Contributors 
+
+Arthur Henningfield
+
+
+## License
+
+See repo
+
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,5 @@
 var city = "Wahkon";
+var state = "MN";
 var forecast = [];
 var requestUrl = `https://api.zippopotam.us/us/mn/wahkon`
 var todayC = (`#todayW`)
@@ -9,61 +10,47 @@ var longitude
 var dataChain2
 
 function updateWeather(searchQ){
-console.log (searchQ)
-var tempCity = searchQ
-    // var thiscity = searchQ.toLowerCase()
-    if (tempCity === NaN){
-    var tempCity = searchQ.toLowerCase()
-    var commaCheck = tempCity.search(`,`)
-    console.log(commaCheck)  
-    requestUrl = `https://api.zippopotam.us/us/${state}/${city}`
+    if (searchQ.length === 5){
+      if(searchQ > 1){
+        requestUrl = `https://api.zippopotam.us/us/${searchQ}`
+      } 
     }
-    else
-    {
-      console.log('it is a number');
-      if (searchQ.length === 5) 
-        {requestUrl = `https://api.zippopotam.us/us/${searchQ}`
-        console.log ('working zipcode')
-        }
-      else return;
+    else if (searchQ.search(',') > -1){
+      city = searchQ.slice(0,searchQ.search(','))
+      state = searchQ.slice(searchQ.search(',')+2)
+      requestUrl = `https://api.zippopotam.us/us/${state}/${city}`
     }
+    else return;
+    
 
-    function updateHistory{
-
-      cityHistory = JSON.parse(localStorage.getItem('cityHistory'));
-      if (cityHistory === null) ""
-      else{
-
-       }
-
-
-    }
 
   fetch(requestUrl)
     .then(function (response) {
       return response.json();
     })
     .then(function (data) {
-      // Grab location data
+      //If it can't find the value of United States its in an invalid search
       if (data.country === 'United States'){
-        console.log ('Succeeded input')
       city = data.places[0][`place name`]   
-      var stateab = data.places[0]['state abbreviation']
+      var stateab = data.places[0][`state abbreviation`]
+      if (stateab === undefined) stateab = data[`state abbreviation`]
       longitude =data.places[0].longitude
       latitude = data.places[0].latitude
-
-        var saveCity = [`${city}, ${stateab}`]
+      //saves the fresh city data that works to the local storage      
+      var saveCity = [`${city}, ${stateab}`]
       cityHistory = JSON.parse(localStorage.getItem('cityHistory'));
       if (cityHistory === null) localStorage.setItem('cityHistory', JSON.stringify(saveCity));
       else{
         cityHistory = [...cityHistory, ...saveCity]
+        if (cityHistory.length > 5)cityHistory.shift();
         localStorage.setItem('cityHistory', JSON.stringify(cityHistory));
        }
+      updateHistory();
 
       var dataChain = `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=90c25e8ed0269e4611e2fc4c244375bc&units=imperial`;
-      console.log (dataChain)
       return dataChain;
     }
+    
     })
 
     .then(function(dataChain){
@@ -72,7 +59,6 @@ var tempCity = searchQ
         return response.json();
       })
       .then(function(wData){
-        console.log(wData)
         for (i = 1; i < 6; i++){
           var arrID = (i*8)-4
           var tDate = wData.list[arrID].dt
@@ -81,35 +67,18 @@ var tempCity = searchQ
           var temp = wData.list[arrID].main.temp
           var windS = wData.list[arrID].wind.speed
           var humid = wData.list[arrID].main.humidity
-
-          console.log (date)
-          console.log (weatherT)
-          if (i === 0){
-            var row = `<div class="card card-block" style="width: 100%>
-            <label id="city${i}">${city} - ${date}<img src="https://openweathermap.org/img/wn/${weatherT}.png" width="50" height="50" /></label>
-            <label id="temp${i}">Temperature: ${temp}F</label>
-            <label id="windS${i}">Wind Speed: ${windS}mph</label>
-            <label id="humid${i}">Humidity: ${humid}%</label>
-            </div>
-            `;
-            $(todayC).append(row);
-        
-          }
-          else
-          {
-            var row = `<div class="card card-block" style="width: 19%">
-            <label>${date}</label>
-            <label><img src="https://openweathermap.org/img/wn/${weatherT}.png" width="50" height="50" /></label>
-            <label>Temperature: ${temp}F</label>
-            <label>Wind Speed: ${windS}mph</label>
-            <label>Humidity: ${humid}%</label>
-            </div>
-            `;
+          var row = `<div class="card card-block wetc" style="width: 19%">
+          <label>${date}</label>
+          <label><img src="https://openweathermap.org/img/wn/${weatherT}.png" width="50" height="50" /></label>
+          <label>Temperature: ${temp}F</label>
+          <label>Wind Speed: ${windS}mph</label>
+          <label>Humidity: ${humid}%</label>
+          </div>
+          `;
             $(forecastW).append(row);
-          }
+          
         }
         dataChain2 = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=90c25e8ed0269e4611e2fc4c244375bc&units=imperial`;
-        console.log (dataChain2)
         return dataChain2
       })
     });
@@ -126,7 +95,7 @@ setTimeout(function(){
         var temp = tData.main.temp
         var windS = tData.wind.speed
         var humid = tData.main.humidity
-        var row = `<div class="card card-block" style="width: 100%">
+        var row = `<div class="card card-block wetc" style="width: 100%">
         <label><h1> ${city} - ${date} <img src="https://openweathermap.org/img/wn/${weatherT}.png" width="70 height="70"/></h1></label>
         <label>Temperature: ${temp}F</label>
         <label>Wind Speed: ${windS}mph</label>
@@ -139,12 +108,36 @@ setTimeout(function(){
     
   },1000);
   }
-
+//checks for typing something in the search box
   $('.citySearch').on("click","#sbtn",function(event){
     
     sText = $(this).siblings('.sbox').val()
-    console.log (sText)
-
      updateWeather(sText);
 
   })
+
+//checks for clicking on the saved city/state history
+$('.citySave').on("click",".prevCity", function(event){
+
+  var selectedCity = $(this).text();
+  updateWeather(selectedCity)
+
+
+})
+//This updates history but only allowing the most recent 5 entires and adding the newest one that succeeded. It adds by city/state even if you use zipcode
+  function updateHistory(){
+
+    $(`.prevCity`).remove()
+    $(`.wetc`).remove()
+    var cityHistory = JSON.parse(localStorage.getItem('cityHistory'));
+    if (cityHistory === null) ""
+    else{
+      for (i = 0; i < cityHistory.length; i++){
+        var cityShow = `<p class='prevCity'>${cityHistory[i]}</p>`
+        $(`.citySave`).append(cityShow)
+      }
+    }
+
+
+  }
+  updateHistory();
